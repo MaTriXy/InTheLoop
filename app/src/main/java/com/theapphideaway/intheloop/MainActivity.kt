367adb4d2405892d164f945963f73fe42969e7cd
ianschoenrock <theapphideaway@gmail.com>
2019-02-05 11:06:34 -0500

    Initial commit

---
+++ app/src/main/java/com/theapphideaway/intheloop/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.theapphideaway.intheloop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import com.theapphideaway.intheloop.fragments.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+
+        tab_layout.setupWithViewPager(view_pager)
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+
+        // Set up the ViewPager with the sections adapter.
+        view_pager.adapter = mSectionsPagerAdapter
+    }
+
+    class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            return when (position) {
+                0 -> FeaturedPage()
+                1 -> UsPage()
+                2 -> WorldPage()
+                3 -> BusinessPage()
+                4 -> TechPage()
+                5 -> EntertainmentPage()
+                6 -> SportsPage()
+                7 -> SciencePage()
+
+
+
+                else -> {
+                    return HealthPage()
+                }
+            }
+        }
+
+        override fun getCount(): Int {
+            // Show 2 total pages.
+            return 9
+        }
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return when (position) {
+                0 -> "Featured"
+                1 -> "US"
+                2 -> "World"
+                3 -> "Business"
+                4 -> "Tech"
+                5 -> "Entertainment"
+                6 -> "Sports"
+                7 -> "Science"
+
+                else -> {
+                    return "Health"
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/theapphideaway/intheloop/fragments/HealthPage.kt
@@ -0,0 +1,35 @@
+package com.theapphideaway.intheloop.fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.theapphideaway.intheloop.R
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [HealthPage.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [HealthPage.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class HealthPage : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_health_page, container, false)
+    }
+}
